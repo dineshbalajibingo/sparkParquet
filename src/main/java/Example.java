14b06ee2249224f04379baeabd3cf8b1f2287fa8
dineshbalaji <dineshbalajibingo@gmail.com>
2021-06-30 18:51:38 +0530

    UPdate code

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -7,13 +7,114 @@ import org.apache.spark.sql.SparkSession;
 
 public class Example {
 
-    public static void main(String[] args) {
-        SparkConf sparkConf = new SparkConf().setAppName("JavaSparkSQL");
-        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        SQLContext sqlContext = new SQLContext(ctx);
-        Dataset<Row> usersDF = sqlContext.read().load("src/main/resources/data/userdata1.parquet");
-       // usersDF.select("name", "favorite_color").write().save("namesAndFavColors.parquet");
-        usersDF.show();
+    private String registration_dttm;
+
+    public String getRegistration_dttm() {
+        return registration_dttm;
     }
+
+    public void setRegistration_dttm(String registration_dttm) {
+        this.registration_dttm = registration_dttm;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getIp_address() {
+        return ip_address;
+    }
+
+    public void setIp_address(String ip_address) {
+        this.ip_address = ip_address;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String gender;
+    private String ip_address;
+    private String cc;
+    private String country;
+    private String birthday;
+    private String salary;
+    private String title;
 
 }
--- src/main/java/SparkExample.java
+++ src/main/java/SparkExample.java
@@ -1,8 +1,12 @@
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class SparkExample {
+
+    private static String itemValue;
     public static void main(String[] args) {
     SparkSession spark = SparkSession
             .builder()
@@ -12,24 +16,32 @@ public class SparkExample {
             .getOrCreate();
 
            runBasicDataSourceExample(spark);
+        System.out.println("Printing Outside Obj " +itemValue);
         spark.stop();
 
         }
     @SuppressWarnings("unchecked")
     private static void runBasicDataSourceExample(SparkSession spark) {
 
-        Dataset<Row> parquetFileDF = spark.read().load("src/main/resources/data/userdata.parquet");
-        parquetFileDF.createOrReplaceTempView("parquetFile");
-
-        Dataset<Row> namesDF = spark.sql("SELECT last_name FROM parquetFile where id=2");
-
-        //namesDF.write().json("C:\\Users\\dinesven\\Desktop\\B!nGo\\ubs-create-instrument\\src\\main\\resources\\data\\spark.json");
-     /*   Dataset<String> namesDS = namesDF.map(
-                (MapFunction<Row, String>) row -> "id: " + row.getString(1),
-                Encoders.STRING());*/
-
+       Dataset<Row> parquetFileDF = spark.read().load("src/main/resources/data/userdata.parquet");
+       parquetFileDF.createOrReplaceTempView("parquetFile");
+        Dataset<Row>  namesDF = spark.sql("SELECT email FROM parquetFile where id=2");
+        System.out.println(namesDF.schema());
         namesDF.show();
+        for(Iterator<Row> iter = namesDF.toLocalIterator(); iter.hasNext();) {
+           String item = (iter.next()).toString();
+            System.out.println("Actual Value is *********" +item);
+            itemValue = item.toString().substring(1, item.length() - 1);
+            System.out.println("*****************"+ itemValue);
+        }
+        if(itemValue.equalsIgnoreCase("afreeman1@is.gd"))
+        {
+            System.out.println("Success !!!!!");
+        }else{
+            System.out.println("Nop !!!!!");
+        }
     }
+
     }
 
 
